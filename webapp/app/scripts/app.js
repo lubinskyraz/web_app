f476064d9fc240da78bc6c14bbdafb071b77f696
razlubinsky <razlubinsky@gmail.com>
2013-07-29 14:36:39 +0300

    my first commit

---
+++ webapp/app/scripts/app.js
@@ -0,0 +1,8 @@
+var Webapp = window.Webapp = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/routes/*');
+require('scripts/controllers/*');
+require('scripts/models/*');
+require('scripts/views/*');
+require('scripts/router');
---
+++ webapp/app/scripts/router.js
@@ -0,0 +1,5 @@
+Webapp.Router.map(function () {
+  
+});
+
+
